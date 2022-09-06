c5e4e913a0b85505b0cc19c492e3d638618ab972
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-06 20:25:48 +0200

    feat: add profile

--- zad3_User/users/views.py
+++ zad3_User/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -15,3 +16,8 @@ def register(request):
         form = UserRegisterForm()
 
     return render(request, "users/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
