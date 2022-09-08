b8126348df7afcceae65b4063ee5a3854be4a2e8
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-08 18:09:51 +0200

    feat: add new api views

--- zad3_User/API/models.py
+++ zad3_User/API/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class Book(models.Model):
+
     title = models.CharField(max_length=100)
-    authors = models.CharField(max_length=100)
-    published_data = models.IntegerField()
-    categories = models.CharField(max_length=100)
-    thumbnail = models.TextField()
+    authors = models.CharField(max_length=100, null=True)
+    published_date = models.IntegerField(null=True)
+    categories = models.CharField(max_length=100, null=True)
+    thumbnail = models.TextField(null=True)
+    book_id = models.CharField(max_length=20, null=True)
 
